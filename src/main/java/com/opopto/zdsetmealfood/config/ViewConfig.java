88f22aa3e6c031e494a6d58d7ed795c1e2da7f9a
jone <huolifu@opopto.com>
2018-07-28 02:44:07 +0800

    删除无用代码

--- src/main/java/com/opopto/zdsetmealfood/config/ViewConfig.java
+++ src/main/java/com/opopto/zdsetmealfood/config/ViewConfig.java
@@ -11,10 +11,8 @@ public class ViewConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry){
         //registry.addViewController("/").setViewName("index");
         registry.addViewController("/list").setViewName("list");
-        registry.addViewController("/edit").setViewName("edit");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/error").setViewName("404");
-        registry.addViewController("/print").setViewName("print");
     }
 
 }
