8fac890a86b9d3d2946484fa3da926518ea73f55
jianghaifu@opopto.com <jianghaifu@opopto.com>
2018-08-13 20:47:14 +0800

    打印修改第三版

--- src/main/resources/static/js/index_model.js
+++ src/main/resources/static/js/index_model.js
@@ -115,33 +115,33 @@ function storage(date,book,place,model_dineType,price,startTable,standardName){
 function printFood(date,book,place,model_dineType,num,price) {
     //清空当前元素所属的页面，再添加上相应的属性
     //跳转页面进行打印，跳转只是增加页面内容
-    $("#printTable").append("<tr><td>军转大酒店菜单</td></tr>");
-    $("#printTable").append("<tr><td>------------------------</td></tr>");
-    $("#printTable").append("<tr><td>时间："+date+"</td></tr>");
-    $("#printTable").append("<tr><td>餐型："+book+"</td></tr>");
-    $("#printTable").append("<tr><td>地点："+place+"</td></tr>");
-    $("#printTable").append("<tr><td>类别："+model_dineType+"</td></tr>");
-    $("#printTable").append("<tr><td>标准："+price+"元/桌</td></tr>");
-    $("#printTable").append("<tr><td>桌数："+num+"桌</td></tr>");
-    $("#printTable").append("<tr><td>-------------------------</td></tr>");
-
-    var table = document.getElementById("printTable");
+    $(".time").append("时间："+date);
+    $(".foodType").append("餐型："+book);
+    $(".place").append("地点："+place);
+    $(".type").append("类别："+model_dineType);
+    $(".price").append("标准："+price+"元/桌");
+    $(".num").append("桌数："+num+"桌");
+    
     var bts = window.localStorage.getItem("bts");
     if(bts != null&& bts !="null"){
         var bts_array = bts.split(',');
         if(bts_array!=null && bts_array.length>0){
-            for(var k in bts_array){
-                if(bts_array[k].length>0){
-                    var newRow = table.insertRow();
-                    var cell = newRow.insertCell(0);
-                    cell.innerHTML=bts_array[k];
+            var k=0;
+            for (var i=0;i<bts_array.length;){
+                $("#printTable").append("<tr class='table"+i+"'></tr>");
+                k=i;
+                for(var j=0;j<2;j++){
+                    if(bts_array[i]){
+                        $(".table"+k+"").append("<td>"+bts_array[i]+"</td>")
+                        ++i;
+                    }
                 }
             }
         }
     }
 
     CreateTwoFormPage();
-    LODOP.PRINT();
+    LODOP.PREVIEW();
 
 }
 
@@ -150,6 +150,14 @@ $("#printBtn").on("click",function () {
     //将需要提交的数据获取
     $("#showError").empty();
     $("#printTable").empty();
+    $(".time").empty();
+    $(".foodType").empty();
+    $(".place").empty();
+    $(".type").empty();
+    $(".price").empty();
+    $(".num").empty();
+
+    //获取当前所有的选中选项
     var date=$("#date_input").val();
     var dateFood=$("#foodType").find("option:selected").text();
     var place=$("#model_chageplace").find("option:selected").text();
@@ -174,38 +182,48 @@ $("#printBtn").on("click",function () {
         $("#showError").empty();
         $("#showError").append("<span>未添加桌数！</span>");
     }else{
-        $("#printTable").append("<tr><td>军转大酒店菜单</td></tr>");
-        $("#printTable").append("<tr><td>------------------------</td></tr>");
-        $("#printTable").append("<tr><td>时间："+date+"</td></tr>");
-        $("#printTable").append("<tr><td>餐型："+dateFood+"</td></tr>");
-        $("#printTable").append("<tr><td>地点："+place+"</td></tr>");
-        $("#printTable").append("<tr><td>类别："+model_dineType+"</td></tr>");
-        $("#printTable").append("<tr><td>标准："+price+"元/桌</td></tr>");
-        $("#printTable").append("<tr><td>桌数："+num+"桌</td></tr>");
-        $("#printTable").append("<tr><td>------------------------</td></tr>");
+        // $("#printTable").append("<tr><td style='padding: 0px'>时间："+date+"</td><td style='padding: 0px'>餐型："+dateFood+"</td></tr>");
+        $(".time").append("时间："+date);
+        $(".foodType").append("餐型："+dateFood);
+        $(".place").append("地点："+place);
+        $(".type").append("类别："+model_dineType);
+        $(".price").append("标准："+price+"元/桌");
+        $(".num").append("桌数："+num+"桌");
+        var foods=new Array();
+        var j=0;
         $("table#model_table").find("td").each(function () {
-            $("#printTable").append("<tr><td>"+$(this).text()+"</td></tr>");
+            foods[j]=$(this).text();
+            j++;
         });
+        var k=0;
+        for (var i=0;i<foods.length;){
+           $("#printTable").append("<tr class='table"+i+"'></tr>");
+           k=i;
+           for(var j=0;j<2;j++){
+               if(foods[i]){
+                   $(".table"+k+"").append("<td>"+foods[i]+"</td>")
+                   ++i;
+               }
+            }
+        }
 
         CreateTwoFormPage();
-        LODOP.PRINT();
+        LODOP.PREVIEW();
     }
 });
 
 function CreateTwoFormPage() {
-
     LODOP=getLodop();
-    LODOP.PRINT_INIT(10,10,1280,453,"军转大酒店菜单");
-    LODOP.ADD_PRINT_TEXT(21,300,151,30,"jz.banana96.com");
+    // LODOP.ADD_PRINT_TEXT(21,300,151,30,"jz.banana96.com");
+    // ADD_PRINT_TEXT(Top,Left,Width,Height,strContent)
     // ADD_PRINT_LINE(Top1,Left1, Top2, Left2,intLineStyle, intLineWidth)
-    LODOP.SET_PRINT_STYLEA(0,"FontSize",12);
+    LODOP.SET_PRINT_STYLEA(0,"FontSize",16);
     LODOP.SET_PRINT_STYLEA(0,"Bold",1);
     LODOP.SET_PRINT_STYLEA(0,"ItemType",1);
     LODOP.SET_PRINT_STYLEA(0,"Horient",2);
     LODOP.SET_PRINT_STYLEA(0,"Vorient",3);
-    LODOP.SET_PRINT_STYLEA(-1,"FontSize",20);
     // ADD_PRINT_HTML(Top,Left,Width,Height, strHtmlContent)
-    LODOP.ADD_PRINT_HTM(50,40,"100%","100%",document.getElementById("print").innerHTML);
+    LODOP.ADD_PRINT_HTM(50,0,"80mm","100%",document.getElementById("print").innerHTML);
 
 }
 
